c6bdd67abc8bba41e782675f9c6b371ef1481c5f
Uli Raich <uli.taich@cern.ch>
2019-04-27 11:14:41 +0200

    adde the examples

---
+++ examples/Example-02-ReceiveData.py
@@ -0,0 +1,26 @@
+import cayenne.client
+import logging
+
+# Cayenne authentication info. This should be obtained from the Cayenne Dashboard.
+MQTT_USERNAME  = "MQTT_USERNAME"
+MQTT_PASSWORD  = "MQTT_PASSWORD"
+MQTT_CLIENT_ID = "MQTT_CLIENT_ID"
+
+
+# The callback for when a message is received from Cayenne.
+def on_message(message):
+    print("message received: " + str(message))
+    cmdMessage=cayenne.client.CayenneMessage(message[0],message[1])  # message[0]: topic, message[1]:payload
+    print("Channel: %d"%cmdMessage.channel)
+    print("value: %s"%cmdMessage.value)
+    # If there is an error processing the message return an error string, otherwise return nothing.
+
+client = cayenne.client.CayenneMQTTClient()
+client.on_message = on_message
+
+client.begin(MQTT_USERNAME, MQTT_PASSWORD, MQTT_CLIENT_ID, loglevel=logging.INFO)
+#client.begin(MQTT_USERNAME, MQTT_PASSWORD, MQTT_CLIENT_ID)
+# For a secure connection use port 8883 when calling client.begin:
+# client.begin(MQTT_USERNAME, MQTT_PASSWORD, MQTT_CLIENT_ID, port=8883, loglevel=logging.INFO)
+client.loop_forever()
+
